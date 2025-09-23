93b68dd7179c01088d19749a9fd8681807670e11
yigitb-dev <bicakci42@yahoo.com>
2025-09-23 23:34:14 +0200

    Add AuthKit class to package init
    Introduces an AuthKit class in authkit/__init__.py that aggregates Encryption, Storage, Manager, OAuthClient, and TwoFA components for easier access. Also updates import to use TwoFA instead of twofa.

--- authkit/__init__.py
+++ authkit/__init__.py
@@ -2,6 +2,14 @@ from .encryption import Encryption
 from .storage import Storage
 from .manager import Manager
 from .oauth import OAuthClient
-from .twofa import twofa 
+from .twofa import TwoFA 
 
 __all__ = ["Encryption", "Storage","Manager","OAuthClient","TwoFA"]
+
+class AuthKit:
+    def __init__(self):
+        self.encryption = Encryption()
+        self.storage = Storage()
+        self.manager = Manager()
+        self.oauth_client = OAuthClient()
+        self.twofa = TwoFA()
